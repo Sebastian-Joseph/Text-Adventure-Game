0e23e643d74ae42e49cccd37798ce7e5431ac9b7
Sebastian Joseph <theclimax25@gmail.com>
2021-03-26 13:33:24 -0400

    ok i got bored of this

--- AdventureGame.py
+++ AdventureGame.py
@@ -9,7 +9,10 @@ if answer == "yes":
 
     if answer == "wake up":
         answer = input("Now you're awake! Will you go outside or brush your teeth? ").lower()
-
+        if answer == "go outside":
+            answer = input("Smart kid, now that you're outside, you can smell the roses. A friend of yours greets you. What will you do? Greet her back or ignore her")
+            if answer == "greet her back":
+                answer = input("Ok ladies man")
     else:
         print("Welp you stayed asleep, goodnight!")
 else:
